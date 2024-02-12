459f83e9bad9f9f12337de2d8eb6081f3ef24175
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-12 23:07:37 +0800

    Create a2024-ingress.tf

---
+++ a2024-ingress.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_manifest" "ingress_a2024_a2024_ingress" {
+  manifest = {
+    "apiVersion" = "networking.k8s.io/v1"
+    "kind" = "Ingress"
+    "metadata" = {
+      "annotations" = {
+        "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+        "nginx.ingress.kubernetes.io/ssl-redirect" = "false"
+      }
+      "name" = "a2024-ingress"
+      "namespace" = "a2024"
+    }
+    "spec" = {
+      "ingressClassName" = "nginx"
+      "rules" = [
+        {
+          "http" = {
+            "paths" = [
+              {
+                "backend" = {
+                  "service" = {
+                    "name" = "a2024-service"
+                    "port" = {
+                      "number" = 1741
+                    }
+                  }
+                }
+                "path" = "/"
+                "pathType" = "Prefix"
+              },
+            ]
+          }
+        },
+      ]
+    }
+  }
+}
