879f411924a89875db36bde061ec7bc77ba27c23
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-12 17:06:50 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
 }
 
