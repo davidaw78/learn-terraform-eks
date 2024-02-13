8cbb70f0cf9c4476e14641876980f13146d24241
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-13 22:33:51 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -222,6 +222,22 @@ resource "aws_iam_role_policy_attachment" "terraform-eks-demo-cluster-AmazonEKSS
   role       = "${aws_iam_role.terraform-eks-demo-role.name}"
 }
 
+# Setup cluster
+resource "aws_eks_cluster" "terraform-eks-demo-cluster" {
+  name            = var.cluster-name
+  role_arn        = "${aws_iam_role.terraform-eks-demo-role-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
+    subnet_ids         = ["${aws_subnet.demo.*.id}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy"
+  ]
+}
+
 # Create public facing security group
 resource "aws_security_group" "terraform-eks-public-facing-sg" {
   vpc_id = aws_vpc.terraform-eks-vpc.id
