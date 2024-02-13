999012d51cbc52f2b9f00a9f47c703048f7d7642
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-13 20:27:33 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ variable "cluster-name" {
 # Setup VPC layer
 resource "aws_vpc" "terraform-eks-demo-vpc" {
   cidr_block = "10.0.0.0/16" # 65,534 ip addresses
-
+# What is tomap?
   tags = "${
-    map(
+    tomap(
      "Name", "terraform-eks-demo-node-vpc",
      "kubernetes.io/cluster/${var.cluster-name}", "shared",
     )
