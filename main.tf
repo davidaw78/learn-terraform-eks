182efcc659ea221f1bfa6ed0d01f111ba791f6aa
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-13 13:41:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ output "endpoint" {
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = data.aws_eks_cluster.demo.certificate_authority[0].data
+  value = aws_eks_cluster.demo.certificate_authority[0].data
 }
 
 # Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
 output "identity-oidc-issuer" {
-  value = data.aws_eks_cluster.demo.identity[0].oidc[0].issuer
+  value = aws_eks_cluster.demo.identity[0].oidc[0].issuer
 }
 
 resource "aws_vpc" "main" {
