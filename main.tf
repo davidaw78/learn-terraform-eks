9973b2aa8416d9c638e9ef0a2db78c79a8d9acd0
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-13 23:12:57 +0800

    308. Added kubeconfig = <<KUBECONFIG

--- main.tf
+++ main.tf
@@ -304,3 +304,39 @@ resource "aws_security_group" "terraform-eks-private-facing-sg" {
     Name = "terraform-eks-private-facing-sg"
   }
 }
+
+# Create kubeconfig. This might help me run kubectl within tf
+locals {
+  kubeconfig = <<KUBECONFIG
+
+
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.terraform-eks-demo.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.terraform-eks-demo.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster-name}"
+KUBECONFIG
+}
+
+output "kubeconfig" {
+  value = "${local.kubeconfig}"
+}
