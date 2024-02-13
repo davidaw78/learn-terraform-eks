fe3a5dc6337cd1be4f7999f40111b157dbeeab64
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-13 10:17:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,61 @@ resource "null_resource" "kubectl" {
   }
 }
 
+resource "kubectl_manifest" "a2024-deployment" {
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: a2024
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: a2024-deployment
+  namespace: a2024
+  labels:
+    app: a2024
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: a2024
+  template:
+    metadata:
+      labels:
+        app: a2024
+    spec:
+      containers:
+        - name: a2024
+          image: elixirtech/elixir-ambience
+          ports:
+            - containerPort: 1740
+          env:
+            - name: externalhost
+#              value: "ec2-44-210-147-25.compute-1.amazonaws.com"
+              value: "a80aa0e285bbe4495a414c623d78f393-917726230.us-east-1.elb.amazonaws.com"
+            - name: externalport
+              value: "80"
+            - name: externalprotocol
+              value: "http:"
+            - name: mongourl
+              value: "mongodb://mongodb-service:27017"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: a2024-service
+  namespace: a2024
+spec:
+  selector:
+    app: a2024
+  ports:
+    - protocol: TCP
+      port: 1741
+      targetPort: 1740
+YAML
+}
+
 resource "kubectl_manifest" "mongo-deployment" {
     yaml_body = <<YAML
 apiVersion: apps/v1
