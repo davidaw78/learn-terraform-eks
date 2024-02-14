d31faa579e5ebf20f2cfd03c3bb660c3a646567c
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-14 09:02:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -384,7 +384,7 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
 }
 
 resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.terraform-eks-demo.name
+  cluster_name    = aws_eks_cluster.terraform-eks-cluster.name
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.terraform-eks-nodes-role.arn
 
