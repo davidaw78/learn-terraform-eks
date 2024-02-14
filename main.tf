63df9681937a380ac9d11b6e3bee68da747d84c0
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-14 22:29:55 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -418,7 +418,7 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+#  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 1
