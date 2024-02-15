5e4d7f0adba71c772e1a9cefb2adc295eebb16e4
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-15 17:39:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   }
 }
 
-resource "null_resource" "kubectl" {
+resource "null_resource" "run-kubectl" {
   provisioner "local-exec" {
         command = "aws eks update-kubeconfig --region ${var.region}  --name ${var.cluster-name}"
   }
@@ -31,7 +31,7 @@ resource "null_resource" "run-kubectl1" {
         kubectl apply -f ~/learn-terraform-eks/a2024-ingress.yaml
         EOT
   }
-  depends_on = [resource.null_resource.kubectl]
+  depends_on = [resource.null_resource.run-kubectl]
 }
 
 #change address
@@ -43,7 +43,7 @@ resource "null_resource" "run-kubectl2" {
         kubectl apply -f ~/learn-terraform-eks/a2024-deployment.yaml
         EOT
   }
-  depends_on = [resource.null_resource.kubectl1]
+  depends_on = [resource.null_resource.run-kubectl1]
 }
 
 variable "cluster-name" {
