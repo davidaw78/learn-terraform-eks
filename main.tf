23feaa38348465499db985a3a928b4a226e49496
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-15 21:11:45 +0800

    70. Added  "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"

--- main.tf
+++ main.tf
@@ -61,10 +61,13 @@ variable "region" {
 
 # Setup VPC and Subnet
 resource "aws_vpc" "terraform-eks-vpc" {
+  enable_dns_support = true
+  enable_dns_hostnames = true
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "${var.cluster-name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
 
