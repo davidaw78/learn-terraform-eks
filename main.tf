7036dbd83028cecdd0728f36c7742c9583df0436
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-16 12:31:23 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_nat_gateway" "terraform-eks-nat" {
 resource "aws_subnet" "terraform-eks-public-subnet" {
   vpc_id                  = aws_vpc.terraform-eks-vpc.id
   cidr_block              = var.public_subnet_cidr_blocks.*
-  availability_zone       = element(var.availability_zones, count.index)
+  availability_zone       = var.availability_zones.*
   map_public_ip_on_launch = true
 
   tags = {
@@ -139,8 +139,8 @@ resource "aws_subnet" "terraform-eks-public-subnet" {
 
 resource "aws_subnet" "terraform-eks-private-subnet" {
   vpc_id            = aws_vpc.terraform-eks-vpc.id
-  cidr_block        = element(var.private_subnet_cidr_blocks, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+  cidr_block        = var.private_subnet_cidr_blocks.*
+  availability_zone = var.availability_zones.*
 
   tags = {
     Name = "${var.cluster-name}-private-subnet"
