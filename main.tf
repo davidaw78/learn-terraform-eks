2fa4a542d5cb20e706cf967a11f0847342f6cc42
David Aw <89636227+davidaw78@users.noreply.github.com>
2024-02-17 08:22:48 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -159,17 +159,6 @@ resource "aws_route_table" "terraform-eks-private-rt" {
     {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.terraform-eks-nat.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     },
   ]
 
@@ -185,17 +174,6 @@ resource "aws_route_table" "terraform-eks-public-rt" {
     {
       cidr_block                 = "0.0.0.0/0"
       gateway_id                 = aws_internet_gateway.terraform-eks-igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     },
   ]
 
@@ -277,7 +255,8 @@ resource "aws_security_group" "terraform-eks-public-facing-sg" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
+#    protocol    = "-1"
     cidr_blocks = flatten([var.private-subnet-cidr-blocks, var.public-subnet-cidr-blocks])
 #    cidr_blocks = ["0.0.0.0/0"]
     # Allow traffic from public subnet
